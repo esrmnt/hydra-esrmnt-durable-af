16636a496ecb77409330e72a89c61f5cd1bcc3d2
Nitish Ranjan <nitish.er@gmail.com>
2022-06-18 13:24:03 -0700

    Added DI using .NET Core FW

---
+++ hydra-esrmnt-durable-af/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Hydra.Esrmnt.Durable.AF.Startup))]
+namespace Hydra.Esrmnt.Durable.AF
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+        }
+    }
+}
